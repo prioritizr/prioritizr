245fb4246007cd21bf3f4a50c16d494f66e4805c
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2017-02-18 22:33:58 +1000

    add_locked_out_constraint

--- R/add_locked_out_constraint.R
+++ R/add_locked_out_constraint.R
@@ -1,4 +1,4 @@
-#' @include internal.R Constraint-proto.R
+#' @include internal.R generics.R Constraint-proto.R intersecting_units.R
 NULL
 
 #' @export
@@ -7,28 +7,38 @@ methods::setMethod('add_locked_out_constraint',
   function(x, locked_out) {
     # assert valid arguments
     assertthat::assert_that(inherits(x, 'ConservationProblem'),
-      inherits(locked_out, 'numeric'), isTRUE(all(is.finite(locked_out))), 
-        isTRUE(all(round(locked_out)) == locked_out),
-        isTRUE(max(locked_out) < x$number_of_planning_units()),
+      inherits(locked_out, c('integer', 'numeric')), 
+        isTRUE(all(is.finite(locked_out))), 
+        isTRUE(all(round(locked_out) == locked_out)),
+        isTRUE(max(locked_out) <= x$number_of_total_units()),
         isTRUE(min(locked_out) >= 1))
     # create parameters 
-    pu_status <- rep(0L, length(x$number_of_planning_units()))
-    pu_status[as.integer(locked_out)] <- 1L
-    p <- parameters(binary_parameter('Apply constraint?', 1L),
-    binary_parameter_array('Planning units', pu_status,
-      as.character(seq_len(x$number_of_planning_units()))))
+    p <- parameters(binary_parameter('apply constraint?', 1L))
     # create new constraint object
     x$add_constraint(pproto(
       'LockedOutConstraint',
       Constraint,
       name='Locked out planning units',
+      data=list(locked_out=as.integer(locked_out)),
       parameters=p,
       apply = function(self, x, y) {
-        assertthat::assert_that(inherits(x$ptr, 'OptimizationProblem'),
+        assertthat::assert_that(inherits(x, 'OptimizationProblem'),
           inherits(y, 'ConservationProblem'))
-        if (self$parameters$get('apply constraint?')==1)
-          invisible(rcpp_apply_locked_out_constraint(x$ptr, 
-            self$parameters$get('planning units')[[1]]))
+        if (self$parameters$get('apply constraint?')==1) {
+          # get locked in units
+          units <- self$data$locked_out
+          # if cost layer is a raster convert cell indices to relative 
+          # indices based on which cells in the cost layer are 
+          # finite (ie. not NA)
+          if (inherits(y$data$cost, 'Raster')) {
+            units <- match(units, raster::Which(!is.na(y$data$cost), 
+              cells=TRUE))
+            units <- units[!is.na(units)]
+          }
+          # apply constraint
+          invisible(rcpp_apply_locked_out_constraint(x$ptr, units))
+        }
+        invisible(TRUE)
       }))
     # return problem
     return(x)
@@ -42,6 +52,8 @@ methods::setMethod('add_locked_out_constraint',
     # assert valid arguments
     assertthat::assert_that(inherits(x, 'ConservationProblem'),
       assertthat::is.string(locked_out),
+      inherits(x$data$cost, 'Spatial'), 
+      isTRUE('data' %in% slotNames(x$data$cost)),
       isTRUE(locked_out %in% names(x$data$cost)),
       isTRUE(inherits(x$data$cost[[locked_out]], 'logical')))
     # add constraint
@@ -57,10 +69,9 @@ methods::setMethod('add_locked_out_constraint',
     assertthat::assert_that(inherits(x, 'ConservationProblem'),
       inherits(locked_out, 'Spatial'))
     # add constraints
-    add_locked_out_constraint(x, intersecting_units(x$cost$data, locked_out))
+    add_locked_out_constraint(x, intersecting_units(x$data$cost, locked_out))
   }
 )
-  
 
 #' @export
 methods::setMethod('add_locked_out_constraint', 
@@ -68,7 +79,8 @@ methods::setMethod('add_locked_out_constraint',
   function(x, locked_out) {
     # assert valid arguments
     assertthat::assert_that(inherits(x, 'ConservationProblem'),
-      inherits(locked_out, 'Raster'))
-    # add constraints
-    add_locked_out_constraint(x, intersecting_units(x$cost$data, locked_out))
-})
+      inherits(locked_out, 'Raster'), 
+      isTRUE(raster::cellStats(locked_out, 'sum') > 0))
+    add_locked_out_constraint(x, intersecting_units(x$data$cost, locked_out))
+  }
+)
---
+++ tests/testthat/test_locked_out_constraint.R
@@ -0,0 +1,172 @@
+context('locked out constraint')
+
+test_that('integer locked out data', {
+  # create problem
+  data(sim_pu_raster, sim_features)
+  p <- problem(sim_pu_raster, sim_features) %>%
+    add_minimum_set_objective() %>%
+    add_relative_targets(0.1) %>%
+    add_binary_decision() %>%
+    add_locked_out_constraint(1:20)
+  o <- compile(p)
+  # check that constraints added correctly
+  locked_out_cells <- 1:20
+  locked_out_indices <- match(locked_out_cells, 
+    raster::Which(!is.na(sim_pu_raster), cells=TRUE))
+  locked_out_indices <- locked_out_indices[!is.na(locked_out_indices)]
+  expect_true(isTRUE(all(o$ub()[locked_out_indices] == 0)))  
+  expect_true(isTRUE(all(o$ub()[-locked_out_indices] == 1)))  
+  # check that the solution obeys constraints as expected
+  s <- solve(p)
+  locked_out_units <- locked_out_cells[locked_out_cells %in% 
+    raster::Which(!is.na(s), cells=TRUE)]
+  expect_true(all(s[locked_out_units]==0))
+  # invalid inputs
+  p <- problem(sim_pu_raster, sim_features) %>%
+    add_minimum_set_objective() %>%
+    add_relative_targets(0.1) %>%
+    add_binary_decision()
+  expect_error(p %>% add_locked_out_constraint(-1))
+  expect_error(p %>% add_locked_out_constraint(9.6))
+  expect_error(p %>% add_locked_out_constraint(raster::ncell(sim_pu_raster)+1))
+})
+
+test_that('character locked out data', {
+  # create problem
+  data(sim_pu_polygons, sim_features)
+  p <- problem(sim_pu_polygons, sim_features) %>%
+    add_minimum_set_objective() %>%
+    add_relative_targets(0.1) %>%
+    add_binary_decision() %>%
+    add_locked_out_constraint('locked_out')
+  o <- compile(p)
+  # check that constraints added correctly
+  expect_true(isTRUE(all(o$ub()[which(sim_pu_polygons$locked_out)] == 0)))
+  # check that the solution obeys constraints as expected
+  s <- solve(p)
+  expect_true(all(s$solution[which(sim_pu_polygons$locked_out)] == 0))
+  # invalid inputs
+  expect_error({
+    sim_pu_polygons$locked_out <- as.integer(sim_pu_polygons$locked_out)
+    problem(sim_pu_polygons, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint('locked_out')})
+  expect_error({
+    problem(sim_pu_polygons, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(NA_character_)})
+  expect_error({
+    problem(sim_pu_polygons, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint('column_name_that_doesnt_exist')})
+  expect_error({
+    problem(sim_pu_polygons, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint('cost')})
+})
+
+test_that('raster locked out data', {
+  # create problem
+  data(sim_pu_raster, sim_locked_out_raster, sim_features)
+  p <- problem(sim_pu_raster, sim_features) %>%
+    add_minimum_set_objective() %>%
+    add_relative_targets(0.1) %>%
+    add_binary_decision() %>%
+    add_locked_out_constraint(sim_locked_out_raster)
+  o <- compile(p)
+  # check that constraints added correctly
+  locked_out_cells <- raster::Which(sim_locked_out_raster &
+    !is.na(sim_pu_raster), cells=TRUE)
+  locked_out_indices <- match(locked_out_cells, 
+    raster::Which(!is.na(sim_pu_raster), cells=TRUE))    
+  expect_true(isTRUE(all(o$ub()[locked_out_indices]==0)))
+  expect_true(isTRUE(all(o$ub()[-locked_out_indices]==1)))
+  # check that the solution obeys constraints
+  s <- solve(p)
+  expect_true(all(s[locked_out_cells] == 0))
+  # check that invalid inputs throw errors
+  expect_error({
+    data(sim_locked_out_raster)
+    extent(sim_locked_out_raster) <- c(0,20,0,20)
+    problem(sim_pu_raster, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(sim_locked_out_raster)})
+  expect_error({
+    data(sim_locked_out_raster)
+    sim_locked_out_raster@crs <- sp::CRS('+init=epsg:4326')
+    problem(sim_pu_raster, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(sim_locked_out_raster)})
+  expect_error({
+    data(sim_locked_out_raster)
+    suppressWarnings(sim_locked_out_raster <- raster::setValues(
+      sim_locked_out_raster, NA))
+    problem(sim_pu_raster, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(sim_locked_out_raster)})
+  expect_error({
+    data(sim_locked_out_raster)
+    sim_locked_out_raster <- raster::setValues(sim_locked_out_raster, 0)
+    problem(sim_pu_raster, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(sim_locked_out_raster)})
+})
+ 
+test_that('spatial locked out data', {
+  # create problem
+  data(sim_pu_polygons, sim_features)
+  p <- problem(sim_pu_polygons, sim_features) %>%
+    add_minimum_set_objective() %>%
+    add_relative_targets(0.1) %>%
+    add_binary_decision() %>%
+    add_locked_out_constraint(sim_pu_polygons[sim_pu_polygons$locked_out,])
+  o <- compile(p)
+  # check that constraints added correctly
+  locked_out_units <- which(sim_pu_polygons$locked_out)
+  expect_true(isTRUE(all(o$ub()[locked_out_units]==0)))
+  expect_true(isTRUE(all(o$ub()[-locked_out_units]==1)))
+  # check that the solution obeys constraints
+  s <- solve(p)
+  expect_true(all(s$solution[locked_out_units]==0))
+  # check that invalid inputs throw errors
+  expect_error({
+    data(sim_pu_polygons, sim_features)
+    problem(sim_pu_polygons[1:10,], sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(sim_pu_polygons[50:55,])})
+  expect_error({
+    data(sim_pu_polygons, sim_features)
+    problem(sim_pu_polygons[1:10,], sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(sim_pu_polygons[0,])})
+  expect_error({
+    data(sim_pu_polygons, sim_features)
+    sim_pu_polygons2 <- sim_pu_polygons[1:10,]
+    sim_pu_polygons2@proj4string <- sp::CRS('+init=epsg:4326')
+    problem(sim_pu_polygons, sim_features) %>%
+        add_minimum_set_objective() %>%
+        add_relative_targets(0.1) %>%
+        add_binary_decision()  %>%
+        add_locked_out_constraint(sim_pu_polygons2)})
+})
+
