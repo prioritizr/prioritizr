a4f95a631243065108b7f53029f217a474303e5a
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-08-16 10:07:41 +0100

    tweak pool example to reduce run time

--- R/add_pool_portfolio.R
+++ R/add_pool_portfolio.R
@@ -41,7 +41,7 @@ NULL
 #'
 #' @examples
 #' # set seed for reproducibility
-#' set.seed(500)
+#' set.seed(600)
 #'
 #' # load data
 #' data(sim_pu_raster, sim_features, sim_pu_zones_stack, sim_features_zones)
@@ -49,9 +49,9 @@ NULL
 #' # create minimal problem with pool portfolio
 #' p1 <- problem(sim_pu_raster, sim_features) %>%
 #'       add_min_set_objective() %>%
-#'       add_relative_targets(0.2) %>%
+#'       add_relative_targets(0.05) %>%
 #'       add_pool_portfolio() %>%
-#'       add_default_solver(gap = 0.02, verbose = FALSE)
+#'       add_default_solver(gap = 0, verbose = FALSE)
 #'
 #' # solve problem
 #' s1 <- solve(p1)
@@ -65,7 +65,7 @@ NULL
 #' # create minimal problem with pool portfolio and find the top 5 solutions
 #' p2 <- problem(sim_pu_raster, sim_features) %>%
 #'       add_min_set_objective() %>%
-#'       add_relative_targets(0.5) %>%
+#'       add_relative_targets(0.05) %>%
 #'       add_pool_portfolio(method = 2, number_solutions = 5) %>%
 #'       add_default_solver(gap = 0, verbose = FALSE)
 #'
@@ -81,11 +81,11 @@ NULL
 #' # build multi-zone conservation problem with pool portfolio
 #' p3 <- problem(sim_pu_zones_stack, sim_features_zones) %>%
 #'       add_min_set_objective() %>%
-#'       add_relative_targets(matrix(runif(15, 0.1, 0.2), nrow = 5,
+#'       add_relative_targets(matrix(runif(15, 0.01, 0.05), nrow = 5,
 #'                                   ncol = 3)) %>%
 #'       add_binary_decisions() %>%
 #'       add_pool_portfolio() %>%
-#'       add_default_solver(gap = 0.02, verbose = FALSE)
+#'       add_default_solver(gap = 0, verbose = FALSE)
 #'
 #' # solve the problem
 #' s3 <- solve(p3)
