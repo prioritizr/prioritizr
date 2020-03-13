d5a5344d5fab02bc5adb849f1a97e9c0dbdc25f2
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2020-03-13 19:18:17 +1300

    fix cran checks failing on ci

--- R/rij_matrix.R
+++ R/rij_matrix.R
@@ -4,8 +4,7 @@ NULL
 #' Feature by planning unit matrix
 #'
 #' Generate a matrix showing the amount of each feature in each planning
-#' unit (also known as an \emph{rij} matrix).Each row corresponds
-#' to a different feature and each column corresponds to a different feature.
+#' unit (also known as an \emph{rij} matrix).
 #'
 #' @param x \code{\link[raster]{Raster-class}},
 #'   \code{\link[sp]{Spatial-class}}, or \code{\link[sf]{sf}} object
@@ -16,33 +15,25 @@ NULL
 #'
 #' @param fun \code{character} for summarizing values inside each planning unit.
 #'   This parameter is only used when the argument to \code{x} is a
-#'   \code{\link[sp]{Spatial-class}} object. Defaults to \code{"sum"}.
+#'   \code{\link[sp]{Spatial-class}} or \code{\link[sf]{sf}} object.
+#'   Defaults to \code{"sum"}.
 #'
 #' @param ... not used.
 #'
-#' @details The sparse matrix represents the spatial intersection between the
-#'   planning units and the features. Rows correspond to planning units,
-#'   and columns correspond to features. Values correspond to the amount
-#'   of the feature in the planning unit. For example, the amount of the
-#'   third species in the second planning unit would be contained in the
-#'   cell in the third column and in the second column.
-#'
-#'   This function can take a long to run for big data sets. To reduce
-#'   processing time, the \code{\link{set_number_of_threads}} function
-#'   can be used to allocate more computational resources. Additionally,
-#'   dealing with planning units represented with
-#'   \code{\link[sp]{SpatialPolygonsDataFrame}} object, the
-#'   \code{\link[velox]{velox}} package can be installed to reduce
-#'   processing time.
-#'
-#'   Generally, processing \code{\link[sp]{Spatial-class}} data takes much
-#'   longer to process then \code{\link[raster]{Raster-class}} data, and
+#' @details
+#'   Generally, processing vector (i.e. \code{\link[sp]{Spatial-class}} or
+#'   \code{\link[sf]{sf}}) data takes much
+#'   longer to process then \code{\link[raster]{Raster-class}} data,
 #'   so it is recommended to use \code{\link[raster]{Raster-class}} data
 #'   for planning units where possible.
 #'
-#' @seealso \code{\link{set_number_of_threads}}, \code{\link[velox]{velox}}.
-#'
-#' @return \code{\link{Matrix}{dgCMatrix-class}} object.
+#' @return \code{\link{Matrix}{dgCMatrix-class}} sparse matrix object.
+#'   The sparse matrix represents the spatial intersection between the
+#'   planning units and the features. Rows correspond to planning units,
+#'   and columns correspond to features. Values correspond to the amount
+#'   of the feature in the planning unit. For example, the amount of the
+#'   third species in the second planning unit would be stored in the
+#'   third column and second row.
 #'
 #' @name rij_matrix
 #'
