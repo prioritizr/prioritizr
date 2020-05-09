efc36adcac13d3f88ef28449d66f423381d2426f
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2020-05-10 11:32:58 +1200

    minor docs tweaks + add more details to irreplaceabiltiy man page + tweak grammar in prioritizr vign + rebuild vigns

--- R/irreplaceability.R
+++ R/irreplaceability.R
@@ -7,25 +7,53 @@ NULL
 #' of planning units in a solution to a conservation planning
 #' \code{\link{problem}}.
 #'
-#' @details The following methods are available for calculating
-#'   irreplaceability scores:
-#'   \describe{
+#' @details
+#' The following methods are available for calculating irreplaceability scores:
 #'
-#'   \item{\code{\link{replacement_cost}}}{Calculate irreplaceability scores
-#'     based on the replacement cost method described in Cabeza and Moilanen
-#'     (2006). This method is recommended for calculating irreplaceability
-#'     scores.}
+#' \describe{
 #'
-#'   \item{\code{\link{ferrier_score}}}{Calculate irreplaceability scores
-#'     using the method outlined in Ferrier \emph{et al.} (2000).}
+#' \item{\code{\link{replacement_cost}}}{
+#'   The replacement cost scores (based
+#'   on Cabeza and Moilanen 2006) quantify the change in the objective
+#'   function (e.g. additional costs required to meet feature targets) of the
+#'   optimal solution if a given planning unit in a solution cannot be acquired.
+#'   They can (i) account for the cost of different planning units, (ii) account
+#'   for multiple management zones, (iii) apply to any objective function, and
+#'   (iv) identify truly irreplaceable planning units (denoted with infinite
+#'   values).}
 #'
-#'   \item{\code{\link{rarity_weighted_richness}}}{Calculate irreplaceability
-#'     scores using rarity weighted richness. This method is only recommended
-#'     for particularly large-scale conservation planning problems where
-#'     the replacement cost method would take too long to produce scores in
-#'     a feasible period of time.}
+#' \item{\code{\link{ferrier_score}}}{
+#'  The Ferrier scores (Ferrier \emph{et al.} 2000) quantify the importance of
+#'  planning units for meeting feature targets. They can only be applied to
+#'  conservation problems with a minimum set objective and a single zone (i.e.
+#'  the classic \emph{Marxan}-type problem). Furthermore---unlike the
+#'  replacement cost scores---the Ferrier irreplaceability scores provide a
+#'  score for each feature within each planning unit, providing insight into
+#'  why certain planning units are more important than other planning units.}
 #'
-#'   }
+#' \item{\code{\link{rarity_weighted_richness}}}{
+#'  The rarity weighted richness scores (based on Williams \emph{et al.}
+#' 1996) are simply a measure of biological diversity. They do not account for
+#' planning costs, multiple management zones, objective functions, or feature
+#' targets (or weightings). They merely describe the spatial patterns of
+#' biodiversity, and do not account for many of the factors needed to quantify
+#' the importance of a planning unit for achieving conservation goals.}
+#'
+#' }
+#'
+#' Generally speaking, we recommend using replacement cost scores for small and
+#' moderate sized problems (e.g. less than 30,000 planning units) when it is
+#' feasible to do so. It can take a very long time to compute replacement
+#' cost scores, and so it is simply not feasible to compute these scores for
+#' particularly large problems. For moderate and large sized problems (e.g.
+#' more than 30,000 planning units), we recommend using the Ferrier
+#' irreplaceability scores if possible. As mentioned earlier, the Ferrier
+#' irreplaceability scores can only be used for a specific type of conservation
+#' problem. For large sized problems (e.g. more than 100,000 planning units),
+#' we recommend using the rarity weighted richness scores simply because there
+#' is no other choice available. It has been known for decades that such
+#' static measures of biodiversity lead to poor conservation plans
+#' (Kirkpatrick 1983).
 #'
 #' @references
 #' Cabeza M and Moilanen A (2006) Replacement cost: A practical measure of site
@@ -36,7 +64,16 @@ NULL
 #' irreplaceability of areas for achieving a conservation goal, its application
 #' to real-world planning, and a research agenda for further refinement.
 #' \emph{Biological Conservation}, 93: 303--325.
-
+#'
+#' Kirkpatrick, JB (1983) An iterative method for establishing priorities for
+#' the selection of nature reserves: an example from Tasmania.
+#' \emph{Biological Conservation}, 25: 127--134.
+#'
+#' Williams P, Gibbons D, Margules C, Rebelo A, Humphries C, and Pressey RL
+#' (1996) A comparison of richness hotspots, rarity hotspots and complementary
+#' areas for conserving diversity using British birds.
+#' \emph{Conservation Biology}, 10: 155--174.
+#'
 #' @seealso \code{\link{problem}}.
 #'
 #' @examples
