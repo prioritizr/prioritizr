fdd558d649a29227a97cd4fb34241db4d3c974ff
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-02-27 12:59:39 +1300

    fix broken test

--- tests/testthat/test_run_calculations.R
+++ tests/testthat/test_run_calculations.R
@@ -8,22 +8,18 @@ test_that("works", {
     add_min_set_objective() %>%
     add_relative_targets(0.1) %>%
     add_binary_decisions() %>%
-    add_boundary_penalties(3, 0.5) %>%
-    add_default_solver(gap = 0, verbose = FALSE)
+    add_boundary_penalties(3, 0.5)
   p2 <-
     problem(sim_pu_raster, sim_features) %>%
     add_min_set_objective() %>%
     add_relative_targets(0.1) %>%
     add_binary_decisions() %>%
-    add_boundary_penalties(3, 0.5) %>%
-    add_default_solver(gap = 0, verbose = FALSE)
+    add_boundary_penalties(3, 0.5)
   ## run calculations
   run_calculations(p1)
-  ## generate solutions
-  s1 <- solve(p1)
-  s2 <- solve(p2)
-  ## compare solutions
-  expect_is(s1, "RasterLayer")
-  expect_is(s2, "RasterLayer")
-  expect_equal(raster::values(s1), raster::values(s2))
+  ## compile problems
+  o1 <- compile(p1)
+  o2 <- compile(p2)
+  ## tests
+  expect_equal(as.list(o1), as.list(o2))
 })
