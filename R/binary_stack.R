70980a9a3712792aa09e62ee35cb66089ba6dfe2
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2024-09-06 14:44:49 +1200

    update binary_stack() with keep_all param

--- R/binary_stack.R
+++ R/binary_stack.R
@@ -3,14 +3,19 @@ NULL
 
 #' Binary stack
 #'
-#' Convert a [terra::rast()] object containing
-#' integer/categorical values into a raster
-#' object where each layer corresponds to a different integer/categorical
-#' value and pixel values denote the presence/absence of the given
-#' integer/categorical values.
+#' Convert a single-layer [terra::rast()] object that contains integer values
+#' into a multi-layer [terra::rast()] object with pixel values denote the
+#' presence/absence of a given integer value. This is methodology is also known
+#' as "one-hot encoding".
 #'
 #' @param x [terra::rast()] object with a single layer that contains integer
 #'   values.
+#'
+#' @param keep_all `logical` value indicating if all integers should be kept
+#'   in the output. If `TRUE`, the output will contain a layer for each
+#'   sequential integer between 1 and the maximum value in `x`. If `FALSE`,
+#'   the output will only contain layers for integer values present in `x`.
+#'   Defaults to `TRUE.`
 #'
 #' @details
 #' This function is provided to help manage data that encompass
@@ -23,14 +28,16 @@ NULL
 #'
 #' @return A [terra::rast()] object.
 #'
-#' @seealso [category_layer()].
+#' @seealso [category_layer()], [terra::segregate()].
 #'
 #' @examples
 #' # create raster with categorical values
 #' x <- terra::rast(matrix(c(1, 2, 4, 0, NA, 1), nrow = 3))
 #'
 #' # plot the raster
+#' \dontrun{
 #' plot(x, main = "x")
+#' }
 #'
 #' # convert to binary stack
 #' y <- binary_stack(x)
@@ -40,7 +47,7 @@ NULL
 #' plot(y)
 #' }
 #' @export
-binary_stack <- function(x) {
+binary_stack <- function(x, keep_all = TRUE) {
   assert_required(x)
   UseMethod("binary_stack")
 }
@@ -48,25 +55,38 @@ binary_stack <- function(x) {
 #' @rdname binary_stack
 #' @method binary_stack Raster
 #' @export
-binary_stack.Raster <- function(x) {
+binary_stack.Raster <- function(x, keep_all = TRUE) {
   assert(inherits(x, "Raster"))
   cli_warning(raster_pkg_deprecation_notice)
-  raster::stack(binary_stack(terra::rast(x)))
+  raster::stack(binary_stack(terra::rast(x, keep_all = keep_all)))
 }
 
 #' @rdname binary_stack
 #' @method binary_stack SpatRaster
 #' @export
-binary_stack.SpatRaster <- function(x) {
+binary_stack.SpatRaster <- function(x, keep_all = TRUE) {
+  # assert valid arguments
   assert(
     inherits(x, "SpatRaster"),
-    is_numeric_values(x)
+    is_numeric_values(x),
+    assertthat::is.flag(keep_all),
+    assertthat::noNA(keep_all)
   )
+
   # create segregated raster
-  terra::segregate(
+  y <- terra::segregate(
     x,
     classes = seq_len(round(terra::global(x, "max", na.rm = TRUE)[[1]])),
     keep = FALSE,
     round = TRUE
   )
+
+  # if needed, remove empty layers
+  if (!isTRUE(keep_all)) {
+    idx <- which(terra::global(y, "max", na.rm = TRUE)[[1]] > 0)
+    y <- y[[idx]]
+  }
+
+  # return result
+  y
 }
--- tests/testthat/test_binary_stack.R
+++ tests/testthat/test_binary_stack.R
@@ -29,3 +29,18 @@ test_that("Raster", {
     terra::as.data.frame(terra::rast(y))
   )
 })
+
+test_that("keep_all = FALSE", {
+  # create data
+  x <- terra::rast(matrix(c(1, 2, 4, 0, NA, 1)))
+  # convert to binary stack
+  y <- binary_stack(x, keep_all = FALSE)
+  # run tests
+  expect_inherits(y, "SpatRaster")
+  expect_equal(terra::nlyr(y), 3)
+  expect_named(y, c("1", "2", "4"))
+  expect_true(is_comparable_raster(x, y))
+  expect_equal(c(terra::values(y[[1]])), c(1, 0, 0, 0, NA, 1))
+  expect_equal(c(terra::values(y[[2]])), c(0, 1, 0, 0, NA, 0))
+  expect_equal(c(terra::values(y[[3]])), c(0, 0, 1, 0, NA, 0))
+})
