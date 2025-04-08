6d765de7a3cde6f2732bf7d8188f3e087ea00ad5
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2025-04-08 20:03:35 +1200

    improve test cov

---
+++ tests/testthat/roxygen-documentation.R
@@ -0,0 +1,7 @@
+test_that("locked_documentation()",
+  expect_inherits(locked_documentation(), "character")
+})
+
+test_that("solution_format_documentation()",
+  expect_inherits(solution_format_documentation(), "character")
+})
