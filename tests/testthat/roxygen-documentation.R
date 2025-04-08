63d9fce195c87611c3d88ffc83b14a3f6df47203
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2025-04-09 07:04:08 +1200

    fix roxygen doc tests

--- tests/testthat/roxygen-documentation.R
+++
@@ -1,7 +0,0 @@
-test_that("locked_documentation()",
-  expect_inherits(locked_documentation(), "character")
-})
-
-test_that("solution_format_documentation()",
-  expect_inherits(solution_format_documentation(), "character")
-})
