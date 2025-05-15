130800ebc2b0256ac91a6580552d851fa7fe7350
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2025-05-15 12:04:47 +1200

    attempt min shortfall alternative formulation

--- src/rcpp_apply_min_shortfall_objective.cpp
+++ src/rcpp_apply_min_shortfall_objective.cpp
@@ -32,8 +32,7 @@ bool rcpp_apply_min_shortfall_objective(
     ptr->_sense.push_back(Rcpp::as<std::string>(targets_sense[i]));
   for (std::size_t z = 0; z < static_cast<std::size_t>(budget.size()); ++z)
     ptr->_sense.push_back("<=");
-  // add in small negative number to objective for planning unit variables to
-  // break ties in solution and select solution with cheapest cost
+  // add model components for planning unit allocation variables
   for (std::size_t z = 0; z < (ptr->_number_of_zones); ++z) {
     for (std::size_t j = 0; j < (ptr->_number_of_planning_units); ++j) {
       if (Rcpp::NumericMatrix::is_na(costs(j, z))) {
@@ -48,13 +47,13 @@ bool rcpp_apply_min_shortfall_objective(
   if (!ptr->_compressed_formulation)
     for (std::size_t i = 0; i < A_extra_ncol; ++i)
        ptr->_obj.push_back(0.0);
-  // add target totals to convert total amounts to proportions
+  // add weights for feature shortfall variables
   for (std::size_t i = 0; i < n_targets; ++i)
-    ptr->_obj.push_back(targets_value[i] > 1.0e-5 ? 1.0 / targets_value[i] : 0);
+    ptr->_obj.push_back(1.0);
   // add in upper and lower bounds for the decision variables representing if
   // each species is adequately conserved
   for (std::size_t i = 0; i < n_targets; ++i)
-    ptr->_ub.push_back(std::numeric_limits<double>::infinity());
+    ptr->_ub.push_back(1.0);
   for (std::size_t i = 0; i < n_targets; ++i)
     ptr->_lb.push_back(0.0);
   // add in binary variable types for variables representing if each species is
@@ -68,7 +67,7 @@ bool rcpp_apply_min_shortfall_objective(
     ptr->_A_j.push_back((ptr->_number_of_zones *
                         ptr->_number_of_planning_units) + A_extra_ncol + i);
   for (std::size_t i = 0; i < n_targets; ++i)
-    ptr->_A_x.push_back(1.0);
+    ptr->_A_x.push_back(targets_value[i]);
   // add in budget constraints
   if (budget.size() == 1) {
     for (std::size_t i = 0;
