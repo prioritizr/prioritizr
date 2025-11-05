32dd726093857699cb19e699396e9e06fe65b34e
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2025-11-06 10:46:04 +1300

    move standalone solver fns to own file

--- R/standalone-assertions_misc.R
+++ R/standalone-assertions_misc.R
@@ -139,58 +139,6 @@ assertthat::on_failure(is_valid_raw_solution) <- function(call, env) {
   msg
 }
 
-#' Any solvers installed?
-#'
-#' Test if any solvers are installed.
-#'
-#' @details This function tests if any of the following packages are installed:
-#'   \pkg{Rsymphony}, \pkg{lpsymphony}, \pkg{gurobi}.
-#'
-#' @return `logical` value indicating if any solvers are installed.
-#'
-#' @noRd
-any_solvers_installed <- function() {
-  !is.null(default_solver_name())
-}
-
-assertthat::on_failure(any_solvers_installed) <- function(call, env) {
-  c(
-    "No optimization solvers are installed.",
-    "x" = "You must install a solver to generate prioritizations.",
-    "i" = "See {.topic solvers} for options."
-  )
-}
-
-#' Default solver name
-#'
-#' This function returns the name of the default solver. If no solvers are
-#' detected on the system, then a `NULL` object is returned.
-#'
-#' @details This function tests if any of the following packages are installed:
-#'   \pkg{gurobi}, \pkg{cplexAPI}, \pkg{rcbc}, \pkg{highs},
-#'   \pkg{lpsymphony}, \pkg{Rsymphony}.
-#'
-#' @return `character` indicating the name of the default solver.
-#'
-#' @noRd
-default_solver_name <- function() {
-  if (requireNamespace("gurobi", quietly = TRUE)) {
-    return("gurobi")
-  } else if (requireNamespace("cplexAPI", quietly = TRUE)) {
-    return("cplexAPI")
-  } else if (requireNamespace("rcbc", quietly = TRUE)) {
-    return("rcbc")
-  } else if (requireNamespace("highs", quietly = TRUE)) {
-    return("highs")
-  } else if (requireNamespace("lpsymphony", quietly = TRUE)) {
-    return("lpsymphony")
-  } else if (requireNamespace("Rsymphony", quietly = TRUE)) {
-    return("Rsymphony")
-  } else {
-    return(NULL)
-  }
-}
-
 #' Is area unit?
 #'
 #' Assert that a value is a valid unit of measuring area.
