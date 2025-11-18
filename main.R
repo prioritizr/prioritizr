69a3cf7c4bd94bb252d3ead1b09b98d9f50976dd
Sandra Neubert <77614048+sandra-neubert@users.noreply.github.com>
2025-11-18 15:02:13 +0000

    tests for different solvers

--- main.R
+++ main.R
@@ -81,11 +81,105 @@ rel_tol_mat <- matrix(c(0.9, 0.1), nrow = 2, ncol = 1)
 test5 <- add_hierarchical_approach(multi_highs, rel_tol_mat) %>%
   solve() 
 
-#### this doesn't work (rel_tol smaller <0.1): 
-testx <- add_hierarchical_approach(multi_highs, 0.099) %>%
+#### this works now (rel_tol smaller <0.1): 
+testx <- add_hierarchical_approach(multi_highs, 0.001) %>%
   solve()
 
 terra::plot(c(testx, test2))
 
 terra::plot(c(testx, test3))
 terra::plot(c(testx, test4))
+
+
+#####
+
+library(rcbc)
+
+p1 <- problem(sim_pu_raster, sim_features) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(0.1) %>%
+  add_binary_decisions() 
+
+p2 <- problem(sim_pu_raster, sim_features) %>%
+  add_min_shortfall_objective(
+    budget = 0.2 * terra::global(sim_pu_raster, sum, na.rm = TRUE)[[1]]
+  ) %>%
+  add_relative_targets(0.2) %>%
+  add_binary_decisions() 
+
+multi_highs <- multi_problem(p1, p2) %>%
+  add_cbc_solver(gap = 0, verbose = FALSE)
+
+test1 <- add_hierarchical_approach(multi_highs, 0.9) %>%
+  solve()
+test2 <- add_hierarchical_approach(multi_highs, 0.1) %>%
+  solve()
+
+test3 <- p1 %>% solve()
+test4 <- p2 %>% solve()
+
+#these have the ones I expect to be similar
+terra::plot(c(test1, test4))
+terra::plot(c(test2, test3))
+
+# alternative
+rel_tol_mat <- matrix(c(0.9, 0.1), nrow = 2, ncol = 1)
+test5 <- add_hierarchical_approach(multi_highs, rel_tol_mat) %>%
+  solve() 
+
+#### this works now (rel_tol smaller <0.1): 
+testx <- add_hierarchical_approach(multi_highs, 0.001) %>%
+  solve()
+
+terra::plot(c(testx, test2))
+
+terra::plot(c(testx, test3))
+terra::plot(c(testx, test4))
+
+##### takes forever
+
+library(Rsymphony)
+
+p1 <- problem(sim_pu_raster, sim_features) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(0.1) %>%
+  add_binary_decisions() 
+
+p2 <- problem(sim_pu_raster, sim_features) %>%
+  add_min_shortfall_objective(
+    budget = 0.2 * terra::global(sim_pu_raster, sum, na.rm = TRUE)[[1]]
+  ) %>%
+  add_relative_targets(0.2) %>%
+  add_binary_decisions() 
+
+multi_highs <- multi_problem(p1, p2) %>%
+  add_rsymphony_solver(gap = 0, verbose = FALSE)
+
+test1 <- add_hierarchical_approach(multi_highs, 0.9) %>%
+  solve()
+test2 <- add_hierarchical_approach(multi_highs, 0.1) %>%
+  solve()
+
+test3 <- p1 %>% solve()
+test4 <- p2 %>% solve()
+
+#these have the ones I expect to be similar
+terra::plot(c(test1, test4))
+terra::plot(c(test2, test3))
+
+# alternative
+rel_tol_mat <- matrix(c(0.9, 0.1), nrow = 2, ncol = 1)
+test5 <- add_hierarchical_approach(multi_highs, rel_tol_mat) %>%
+  solve() 
+
+#### this works now (rel_tol smaller <0.1): 
+testx <- add_hierarchical_approach(multi_highs, 0.001) %>%
+  solve()
+
+terra::plot(c(testx, test2))
+
+terra::plot(c(testx, test3))
+terra::plot(c(testx, test4))
+
+
+
