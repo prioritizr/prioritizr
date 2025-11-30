a050287e233d796d5cb1aec4a844b5b5e1bd2a43
Sandra Neubert <77614048+sandra-neubert@users.noreply.github.com>
2025-11-30 17:55:26 +0000

    some more tests

--- tests/testthat/test_add_hierarchical_approach.R
+++ tests/testthat/test_add_hierarchical_approach.R
@@ -1,4 +1,4 @@
-test_that("solver information (single solution)", {
+test_that("gurobi solver information (single solution)", {
   skip_on_cran()
   #skip_if_no_solvers_installed()
   # import data
@@ -43,9 +43,9 @@ test_that("solver information (single solution)", {
 })
 
 
-test_that("solver information (multiple solutions)", {
+test_that("gurobi solver information (multiple solutions)", {
   skip_on_cran()
-  skip_if_no_solvers_installed()
+ # skip_if_no_solvers_installed()
   # import data
   sim_zones_pu_raster <- get_sim_zones_pu_raster()
   sim_features <- get_sim_features()
@@ -63,7 +63,7 @@ test_that("solver information (multiple solutions)", {
         add_binary_decisions()
     ) %>%
     add_hierarchical_approach(rel_tol = rel_tol, verbose = FALSE) %>%
-    add_default_solver(gap = 0, verbose = FALSE)
+    add_highs_solver(gap = 0, verbose = FALSE)
   # solve problem
   s <- solve(p)
   # run tests
@@ -86,4 +86,125 @@ test_that("solver information (multiple solutions)", {
   expect_inherits(attr(s, "gap"), "numeric")
   expect_length(attr(s, "gap"), 5)
   expect_named(attr(s, "gap"), names(s))
+})
+
+test_that("other solver information (single solution)", {
+  skip_on_cran()
+ # skip_if_no_solvers_installed()
+  # import data
+  sim_zones_pu_raster <- get_sim_zones_pu_raster()
+  sim_features <- get_sim_features()
+  weights <- runif(2)
+  # create multi-object problem
+  p <-
+    multi_problem(
+      obj1 = problem(sim_zones_pu_raster[[1]], sim_features) %>%
+        add_min_set_objective() %>%
+        add_absolute_targets(seq_along(terra::nlyr(sim_features))) %>%
+        add_binary_decisions(),
+      obj2 = problem(sim_zones_pu_raster[[2]], sim_features) %>%
+        add_min_set_objective() %>%
+        add_absolute_targets(rev(seq_along(terra::nlyr(sim_features)))) %>%
+        add_binary_decisions()
+    ) %>%
+    add_weighted_sum_approach(weights = weights, verbose = FALSE) %>%
+    add_default_solver(gap = 0, verbose = FALSE)
+  # solve problem
+  s <- solve(p)
+  # run tests
+  expect_inherits(s, "SpatRaster")
+  expect_equal(terra::nlyr(s), 1)
+  expect_named(s, names(sim_zones_pu_raster[[1]]))
+  expect_inherits(attr(s, "objective"), "matrix")
+  expect_inherits(c(attr(s, "objective")), "numeric")
+  expect_equal(ncol(attr(s, "objective")), 2)
+  expect_equal(nrow(attr(s, "objective")), 1)
+  expect_equal(colnames(attr(s, "objective")), c("obj1", "obj2"))
+  expect_equal(rownames(attr(s, "objective")), "solution_1")
+  expect_inherits(attr(s, "runtime"), "numeric")
+  expect_length(attr(s, "runtime"), 1)
+  expect_named(attr(s, "runtime"), "solution_1")
+  expect_inherits(attr(s, "status"), "character")
+  expect_length(attr(s, "status"), 1)
+  expect_named(attr(s, "status"), "solution_1")
+  expect_inherits(attr(s, "gap"), "numeric")
+  expect_length(attr(s, "gap"), 1)
+  expect_named(attr(s, "gap"), "solution_1")
+})
+
+test_that("other solver information (multiple solutions)", {
+  skip_on_cran()
+  #skip_if_no_solvers_installed()
+  # import data
+  sim_zones_pu_raster <- get_sim_zones_pu_raster()
+  sim_features <- get_sim_features()
+  weights <- matrix(runif(10), ncol = 2)
+  # create multi-object problem
+  p <-
+    multi_problem(
+      obj1 = problem(sim_zones_pu_raster[[1]], sim_features) %>%
+        add_min_set_objective() %>%
+        add_absolute_targets(seq_along(terra::nlyr(sim_features))) %>%
+        add_binary_decisions(),
+      obj2 = problem(sim_zones_pu_raster[[2]], sim_features) %>%
+        add_min_set_objective() %>%
+        add_absolute_targets(rev(seq_along(terra::nlyr(sim_features)))) %>%
+        add_binary_decisions()
+    ) %>%
+    add_weighted_sum_approach(weights = weights, verbose = FALSE) %>%
+    add_highs_solver(gap = 0, verbose = FALSE)
+  # solve problem
+  s <- solve(p)
+  # run tests
+  expect_inherits(s, "list")
+  expect_length(s, 5)
+  expect_named(s, paste0("solution_", seq_len(5)))
+  expect_true(all_elements_inherit(s, "SpatRaster"))
+  expect_inherits(attr(s, "objective"), "matrix")
+  expect_inherits(c(attr(s, "objective")), "numeric")
+  expect_equal(ncol(attr(s, "objective")), 2)
+  expect_equal(nrow(attr(s, "objective")), 5)
+  expect_equal(colnames(attr(s, "objective")), c("obj1", "obj2"))
+  expect_equal(rownames(attr(s, "objective")), names(s))
+  expect_inherits(attr(s, "runtime"), "numeric")
+  expect_length(attr(s, "runtime"), 5)
+  expect_named(attr(s, "runtime"), names(s))
+  expect_inherits(attr(s, "status"), "character")
+  expect_length(attr(s, "status"), 5)
+  expect_named(attr(s, "status"), names(s))
+  expect_inherits(attr(s, "gap"), "numeric")
+  expect_length(attr(s, "gap"), 5)
+  expect_named(attr(s, "gap"), names(s))
+})
+
+test_that("invalid inputs", {
+  # import data
+  sim_zones_pu_raster <- get_sim_zones_pu_raster()
+  sim_features <- get_sim_features()
+  rel_tol <- matrix(runif(5), ncol = 1)
+  # create multi-object problem
+  p <-
+    multi_problem(
+      problem(sim_zones_pu_raster[[1]], sim_features) %>%
+        add_min_set_objective() %>%
+        add_absolute_targets(seq_along(terra::nlyr(sim_features))) %>%
+        add_binary_decisions(),
+      problem(sim_zones_pu_raster[[2]], sim_features) %>%
+        add_min_set_objective() %>%
+        add_absolute_targets(rev(seq_along(terra::nlyr(sim_features)))) %>%
+        add_binary_decisions()
+    )
+  # run tests
+  expect_tidy_error(
+    add_hierarchical_approach(p, "a"),
+    "numeric"
+  )
+  expect_tidy_error(
+    add_hierarchical_approach(p),
+    "rel_tol"
+  )
+  expect_tidy_error(
+    add_hierarchical_approach(p, c(0.2, 0.3)),
+    "length"
+  )
 })
