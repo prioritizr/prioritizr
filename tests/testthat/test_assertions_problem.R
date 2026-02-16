17aea335a7cdea4217eb6abf5ffa46a3c1cfff9e
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2026-02-16 15:52:06 +1300

    update tests for all_comparable_problem() - these still need work

--- tests/testthat/test_assertions_problem.R
+++ tests/testthat/test_assertions_problem.R
@@ -1,53 +1,53 @@
 test_that("all_comparable_problem (single zone, TRUE)", {
-
+  stop("TODO")
 })
 
 test_that(
   "all_comparable_problem (single zone, different planning unit class)", {
-
+  stop("TODO")
 })
 
 test_that(
   "all_comparable_problem (single zone, different zone names)", {
-
+  stop("TODO")
 })
 
 test_that(
   "all_comparable_problem (single zone, different planning unit ids)", {
-
+  stop("TODO")
 })
 
 test_that(
   "all_comparable_problem (multiple zones, TRUE)", {
-
+  stop("TODO")
 })
 
 test_that(
   "all_comparable_problem (multiple zones, different planning unit class)", {
-
+  stop("TODO")
 })
 
 test_that("all_comparable_problem (multiple zones, different zone names)", {
-
+  stop("TODO")
 })
 
 test_that(
   "all_comparable_problem (multiple zones, different planning unit ids)", {
-
+  stop("TODO")
 })
 
 test_that("assert_pass_presolve_check (TRUE)", {
-
+  stop("TODO")
 })
 
 test_that("assert_pass_presolve_check (error)", {
-
+  stop("TODO")
 })
 
 test_that("verify_pass_presolve_check (TRUE)", {
-
+  stop("TODO")
 })
 
 test_that("verify_pass_presolve_check (error)", {
-
+  stop("TODO")
 })
